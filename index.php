a7b351dcaca675925d511f97bbe16fd95a64af3a
alecasentini <ale.casentini@gmail.com>
2023-05-22 14:25:47 +0200

    Creazione mockup statico

--- index.php
+++ index.php
@@ -13,6 +13,62 @@
 
 <body>
 
+<div class="vh-100 w-100" style="background-color: #001632">
+
+    <h1 class="text-center text-secondary pt-5">Strong Password Generator</h1>
+    <h2 class="text-center text-white">Genera una password sicura</h2>
+
+    <div class="container d-flex bg-white rounded p-3 w-50">
+        <div class="d-flex flex-column w-50">
+            <p>Lunghezza password:</p>
+            <p>Consenti ripetizioni di uno o più caratteri</p>
+            <div class="d-flex mt-5">
+                <button type="button" class="btn btn-primary">Invia</button>
+                <button type="button" class="btn btn-secondary ms-2">Annulla</button>
+            </div>
+        </div>
+        
+        <div class="d-flex flex-column w-50">
+
+            <input type="text" class="form-control w-50">
+
+            <div class="form-check mt-3">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Sì
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">
+                    No
+                </label>
+            </div>
+
+            <div class="form-check mt-3">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Lettere
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Numeri
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Simboli
+                </label>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
